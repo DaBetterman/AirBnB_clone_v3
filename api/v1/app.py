f15f8a6aa66382710ec964db7d5b9b04f19ba5b7
Method Mkoma <gershommethod02@gmail.com>
2024-05-01 07:34:24 +0300

    Task 05: Fixing

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, Blueprint, make_response
 import os
 
 
@@ -20,7 +20,7 @@ def close(exception):
 @app.errorhandler(404)
 def not_found(error):
     """ handling not found """
-    return jsonify({"error": "Not found"}), 404
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
